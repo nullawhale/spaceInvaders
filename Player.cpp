46ce5f18e0f347347fcec65cda93d83633fa8e0d
Nikita <kura98.21@gmail.com>
2016-12-16 13:27:21 +0300

    repush

--- Player.cpp
+++ Player.cpp
@@ -4,6 +4,9 @@
 #include <math.h>
 #include "Player.h"
 #include "DrawBresenhamLine.h"
+#include "MainConst.h"
+
+Player::Player(){}
 
 Player::Player(double _x, double _y, int a){
 	x = _x;
@@ -15,17 +18,26 @@ DrawBresenhamLine dBl;
 
 void Player::drawPlayer(double _x, double _y, int a){
 	//dBl.drawPoint(_x, _y);
+	int pwh = PLAYER_SIZE;
 	glPushMatrix();
 
 	glTranslated(_x, _y, 0);
 	glRotated(a, 0, 0, 1);
 
 	glBegin(GL_LINE_STRIP);
-		glVertex3f(-5, -5, 0);
-		glVertex3f( 0,  5, 0);
-		glVertex3f( 5, -5, 0);
-		glVertex3f(-5, -5, 0);
+		glVertex3f(-pwh, -pwh, 0);
+		glVertex3f( 0,    pwh, 0);
+		glVertex3f( pwh, -pwh, 0);
+		glVertex3f(-pwh+10, -pwh+10, 0);
+		glVertex3f(-pwh, -pwh, 0);
 	glEnd();
 
 	glPopMatrix();
+}
+
+void Player::update(){
+	if (x >= WIDTH_D - PLAYER_SIZE){ x = WIDTH_D - PLAYER_SIZE; }
+	if (x <= PLAYER_SIZE){ x = PLAYER_SIZE; }
+	if (y >= HEIGHT_D - PLAYER_SIZE){ y = HEIGHT_D - PLAYER_SIZE; }
+	if (y <= PLAYER_SIZE){ y = PLAYER_SIZE; }
 }
