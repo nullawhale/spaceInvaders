3660a0a1839a4238a48a3b6849ab55477964c6a8
Nikita <kura98.21@gmail.com>
2016-12-16 14:34:52 +0300

    Bullet v0.1
    Troubles with redrawing

--- Bullet.h
+++ Bullet.h
@@ -7,17 +7,13 @@ public:
 	Bullet(double _x, double _y, int a, int _active);
 	void drawBullet(double _x, double _y, int a);
 	void updateCord();
-	void update();
+	void update(double _x, double _y);
 	void set_curr_player_cord(double _x, double _y);
 
 	double x;
 	double y;
 	int angle;
 	int active;
-
-private:
-	double cur_player_x;
-	double cur_player_y;
 };
 
 #endif // __BULLET__
