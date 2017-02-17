461c4797236b6c1fbd4121b8231352e17804e8ce
nullawhale <kura98.21@gmail.com>
2017-02-17 16:16:56 +0300

    Because of C++11

--- src/MainConst.h
+++ src/MainConst.h
@@ -21,11 +21,8 @@ const int MAX_ASTEROIDS_ON_SCREEN = 2;
 
 #define M_PI 3.14159265358979323846
 
-/*using u8 = unsigned char;
+using u8 = unsigned char;
 using u16 = unsigned short;
-using u32 =  unsigned int;*/
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned int u32;
+using u32 =  unsigned int;
 
 #endif // __CONSTS__
