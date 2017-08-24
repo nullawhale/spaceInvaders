a4faf1a3c4ec76f4e36659dbc6e71c4ecfa46313
nullawhale <kura98.21@gmail.com>
2017-08-24 09:19:08 +0300

    ident with 4 spaces

--- src/Player.h
+++ src/Player.h
@@ -6,24 +6,27 @@
 
 class Player{
 public:
-	Player();
-	Player(bool l, int _hp, double _x, double _y, int a);
-	void drawPlayer();
-	void update(u8 * d);
-	void reset(int _x, int _y);
+    Player();
+    Player(bool l, int _hp, double _x, double _y, int a);
+    void drawPlayer();
+    void update(u8 * d);
+    void reset(int _x, int _y);
+    void KeyboardMove(int key, int _x, int _y);
+    void KeyboardMoveUp(int key, int _x, int _y);
 
-	double x;
-	double y;
-	double dx;
-	double dy;
-	double a; // acceleration
-	int angle; // angle
-	int moving; // 0 - idle; 1 - moving;
-	int slowdown;
-	int left;	 // 1 - left;
-	int right;   // 1 - right;
-	bool life;
-	int hp;
+    double x;
+    double y;
+    double dx;
+    double dy;
+    double a; // acceleration
+    int angle; // angle
+    bool life;
+    int hp;
+private:
+    int moving;
+    int slowdown;
+    int left;
+    int right;
 };
 
 #endif // __PLAYER__
