ecfe64829776d9c4b24a3f385012d481379b4b32
Andrey Manzharov <andreyrmg@gmail.com>
2019-10-03 10:11:20 +0300

    no raw pointers, active is now bool, clean up include headers

--- src/Bullet.h
+++ src/Bullet.h
@@ -1,29 +1,24 @@
 #ifndef __BULLET__
 #define __BULLET__
 
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-#include <math.h>
-#include <cstdio>
-#include <vector>
 #include "CircleBlock.h"
-#include "MainConst.h"
 
-class Bullet{
+#include <memory>
+
+class Bullet {
 public:
-    Bullet();
-    Bullet(double p_x, double p_y, int p_angle);
+    Bullet(double x, double y, int angle);
     void draw();
     void update();
 
     double x;
     double y;
+    int angle;
     double dx;
     double dy;
-    int angle;
-    int active;
+    bool active;
     int hp;
-    CircleBlock* block;
+    std::unique_ptr<CircleBlock> block;
 };
 
 #endif // __BULLET__
