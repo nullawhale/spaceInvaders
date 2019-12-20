fffc63595cc14cf725378bf8f994de0ba7cde788
Andrey Manzharov <andreyrmg@gmail.com>
2019-12-20 16:20:21 +0300

    fix

--- src/LoadTexture.cpp
+++ src/LoadTexture.cpp
@@ -1,5 +1,5 @@
 #define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
+#include <stb/stb_image.h>
 
 #include "LoadTexture.h"
 
@@ -13,14 +13,13 @@ bool LoadTexture::initTexture(const char* filename) {
         return false;
     }
 
-    glCreateTextures(GL_TEXTURE_2D, 1, &texture);
+    glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
     stbi_image_free(data);
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glGenerateMipmap(GL_TEXTURE_2D);
 
     glBindTexture(GL_TEXTURE_2D, 0);
 
